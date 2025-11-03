6c75774056f10474a63f37d458bf7409353f84ac
ALPHA-MOON <leopard37v@gmail.com>
2025-11-03 23:05:42 +0900

    Initial commit

---
+++ src/main/java/com/limitlesscode/aiwonfriendsbackend/AiWonFriendsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.limitlesscode.aiwonfriendsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWonFriendsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiWonFriendsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limitlesscode/aiwonfriendsbackend/AiWonFriendsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limitlesscode.aiwonfriendsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWonFriendsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
